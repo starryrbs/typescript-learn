ea36301da01b2a7e6b6c45c425c3fb2e2fedb067
starryrbs <1322096624@qq.com>
2019-07-26 00:20:33 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class App extends Vue {
+  msg = 123;
+  mounted () {
+    console.log('ads')
+    const a = 'dsa'
+    console.log(a)
+  }
+  static getData () {
+    console.log('112121')
+  }
+}
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
